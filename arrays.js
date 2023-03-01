11d835b255a56cf6f3817c78a268158eedbd0f34
“Saki <sakisandra.c@gmail.com>
2023-02-28 19:41:24 -0700

    Use 3 methods on each array

--- arrays.js
+++ arrays.js
@@ -1,3 +1,12 @@
 var animals = ['dog', 'cat', 'rabbit', 'ferret']
 var ages = [5, 6, 7, 8]
 var isTall = [true, false, true, false]
+
+// this will use the method pop to remove ferret from the animal array
+animals.pop()
+
+//this will add 9 to the end of the animals array
+ages.push(9)
+
+//this will add false to the beginning of the isTall array
+isTall.unshift(false)
