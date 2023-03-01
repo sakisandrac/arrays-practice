3d27ed7d064d03c623f94e56a9a736e26c0cbc35
“Saki <sakisandra.c@gmail.com>
2023-02-28 19:45:26 -0700

    Show an understanding of index positions

--- arrays.js
+++ arrays.js
@@ -10,3 +10,8 @@ ages.push(9)
 
 //this will add false to the beginning of the isTall array
 isTall.unshift(false)
+
+var cars = ['Toyota', 'Tesla', 'Honda', 'Mini', 'BMW']
+//this will use the method slice to change the array from index 1 up to index 3
+//the array will now be 'Tesla', 'Honda', 'Mini'
+cars.slice(1, 4)
